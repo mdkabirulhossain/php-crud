2fbbac3738581f52ad1b79fcb0dc72440ac50013
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-07-02 03:06:47 +0600

    Create file updated

--- create.php
+++ create.php
@@ -3,9 +3,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <title>Add new Book</title>
+    
 </head>
 <body>
-    
+    <div class="container">
+        <header class="d-flex justify-content-between my-4">
+            <h1>Add New Book</h1>
+            <div>
+                <a href="index.php" class="btn btn-primary">Back</a>
+            </div>
+        </header>
+        <form action="process.php" method="post">
+            <div class="form-element my-4">
+                <input type="text" class="form-control" name="title" placeholder='Book Title' id="">
+            </div>
+            <div class="form-element my-4">
+                <input type="text" class="form-control" name="author" placeholder='Author Name: ' id="">
+            </div>
+            <div class="form-element my-4">
+                <select name="type" class="form-control">
+                    <option value="">Select Book Type</option>
+                    <option value="Adventure">Adventure</option>
+                    <option value="Fantasy">Fantasy</option>
+                    <option value="Comedy">Comedy</option>
+                    <option value="Horror">Horror</option>
+                </select>
+            </div>
+            <div class="form-element my-4">
+                <input type="text" class="form-control" name="description" placeholder='Book Description' id="">
+            </div>
+            <div class="form-element">
+                <input type="submit" class="btn btn-success" name="create" value="Add Book">
+            </div>
+        </form>
+    </div>
 </body>
 </html>
